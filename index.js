c2e085ef900a3f9c5ede8f5610ca8736b9fd48a3
Narcis Brindusescu <Narcis75@gmail.com>
2022-07-11 19:33:13 +0300

    se profileaza

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const fs = require('fs');
-const zlib = require('zlib');
+const fsPromises =require('fs/promises')
 
-const directoryFiles = fs.readdirSync('./');
+const directoryFiles = fs.readdirSync('./primite/');
+
 
 const AdmZip = require("adm-zip");
 const path = require("path");
 
 async function extractArchive(filepath) {
   try {
+    //console.log(filepath)
     const zip = new AdmZip(filepath);
-    const outputDir = `./`;
+    const outputDir = `./prelucrate/`;
+  
     zip.extractAllTo(outputDir,true);
 
     console.log(`Extracted to "${outputDir}" successfully`);
@@ -18,8 +21,53 @@ async function extractArchive(filepath) {
   }
 }
 
-directoryFiles.map(fisier=>{
-  if (fisier.substr(fisier.length - 3)==='zip'){
-    extractArchive(fisier);
+
+const emptyFolder = async (folderPath) => {
+  try {
+      // Find all files in the folder
+      const files = await fsPromises.readdir(folderPath);
+      for (const file of files) {
+          await fsPromises.unlink(path.resolve(folderPath, file));
+          console.log(`${folderPath}/${file} has been removed successfully`);
+      }
+      directoryFiles.map(fisier=>{
+        if (fisier.substr(fisier.length - 3)==='zip'){
+          let f = path.join('./primite/', fisier)
+         // console.log(f)
+          extractArchive(f);
+        }
+      })
+
+      const fisiereDePrelucrat = fs.readdirSync('./prelucrate/');
+
+      fisiereDePrelucrat.map(fp=>{
+     
+       if (fp.substr(fp.length - 3)==='xml' && fp.slice(0,4)!=="semn"){
+         let fis = path.join('./prelucrate/', fp)
+         console.log(fis)
+        
+       }
+      })
+
+
+  } catch (err){
+      console.log(err);
   }
-})
+}
+var folder = './prelucrate/';
+   
+/*fs.readdir(folder, (err, files) => {
+  if (err) throw err;
+  
+  for (const file of files) {
+      console.log(file + ' : File Deleted Successfully.');
+      fs.unlinkSync(folder+file);
+  }
+  
+});*/
+
+ emptyFolder(folder);
+
+
+
+
